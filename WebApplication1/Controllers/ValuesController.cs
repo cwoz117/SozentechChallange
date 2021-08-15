8c9d8dd51771f8797a0a486d7ebdbd1eb983d343
Christopher Wozniak <christopher.wozniak@outlook.com>
2021-08-15 10:21:08 -0600

    quick challenge added so sozentech can see the result

---
+++ WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public ValuesController(){}
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            if (id % 3 == 0)
+            {
+                if (id % 5 == 0)
+                {
+                    return "fishchips";
+                }
+                return "fish";
+            } else if (id % 5 == 0)
+            {
+                return "ships";
+            }
+            return "nofish";
+        }
+    }
+}
